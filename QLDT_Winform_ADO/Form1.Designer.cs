6086e68e00912d942e41c0a18f04c4589b71f43c
SitorTasma <100101949+SitorTasma@users.noreply.github.com>
2022-03-19 10:21:04 +0700

    Initial commit

---
+++ QLDT_Winform_ADO/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace QLDT_Winform_ADO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_chonhang = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_gia = new System.Windows.Forms.TextBox();
+            this.checkb_cngia = new System.Windows.Forms.CheckBox();
+            this.btn_capnhat = new System.Windows.Forms.Button();
+            this.dataGV_dt = new System.Windows.Forms.DataGridView();
+            this.MA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenDongDT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DonGia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TonKho = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_xoadongdienthoai = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lb_soluongdt = new System.Windows.Forms.Label();
+            this.nud_tonkho = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV_dt)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_tonkho)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_chonhang
+            // 
+            this.cb_chonhang.FormattingEnabled = true;
+            this.cb_chonhang.Location = new System.Drawing.Point(79, 23);
+            this.cb_chonhang.Name = "cb_chonhang";
+            this.cb_chonhang.Size = new System.Drawing.Size(379, 21);
+            this.cb_chonhang.TabIndex = 0;
+            this.cb_chonhang.SelectedIndexChanged += new System.EventHandler(this.cb_chonhang_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chọn Hãng";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nud_tonkho);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_gia);
+            this.groupBox1.Controls.Add(this.checkb_cngia);
+            this.groupBox1.Controls.Add(this.btn_capnhat);
+            this.groupBox1.Location = new System.Drawing.Point(15, 307);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(443, 85);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cập nhật giá - tồn kho";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tồn kho";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Giá";
+            // 
+            // txt_gia
+            // 
+            this.txt_gia.Location = new System.Drawing.Point(73, 25);
+            this.txt_gia.Name = "txt_gia";
+            this.txt_gia.Size = new System.Drawing.Size(183, 20);
+            this.txt_gia.TabIndex = 2;
+            this.txt_gia.TextChanged += new System.EventHandler(this.txt_gia_TextChanged);
+            // 
+            // checkb_cngia
+            // 
+            this.checkb_cngia.AutoSize = true;
+            this.checkb_cngia.Location = new System.Drawing.Point(332, 31);
+            this.checkb_cngia.Name = "checkb_cngia";
+            this.checkb_cngia.Size = new System.Drawing.Size(86, 17);
+            this.checkb_cngia.TabIndex = 1;
+            this.checkb_cngia.Text = "Cập nhật giá";
+            this.checkb_cngia.UseVisualStyleBackColor = true;
+            this.checkb_cngia.CheckedChanged += new System.EventHandler(this.checkb_cngia_CheckedChanged);
+            // 
+            // btn_capnhat
+            // 
+            this.btn_capnhat.Location = new System.Drawing.Point(332, 54);
+            this.btn_capnhat.Name = "btn_capnhat";
+            this.btn_capnhat.Size = new System.Drawing.Size(105, 23);
+            this.btn_capnhat.TabIndex = 0;
+            this.btn_capnhat.Text = "Cập nhật";
+            this.btn_capnhat.UseVisualStyleBackColor = true;
+            this.btn_capnhat.Click += new System.EventHandler(this.btn_capnhat_Click);
+            // 
+            // dataGV_dt
+            // 
+            this.dataGV_dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGV_dt.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MA,
+            this.TenDongDT,
+            this.DonGia,
+            this.TonKho});
+            this.dataGV_dt.Location = new System.Drawing.Point(15, 59);
+            this.dataGV_dt.Name = "dataGV_dt";
+            this.dataGV_dt.Size = new System.Drawing.Size(443, 208);
+            this.dataGV_dt.TabIndex = 2;
+            this.dataGV_dt.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGV_dt_CellClick);
+            this.dataGV_dt.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGV_dt_CellContentClick);
+            // 
+            // MA
+            // 
+            this.MA.DataPropertyName = "Ma";
+            this.MA.HeaderText = "Mã";
+            this.MA.MinimumWidth = 2;
+            this.MA.Name = "MA";
+            // 
+            // TenDongDT
+            // 
+            this.TenDongDT.DataPropertyName = "TenDT";
+            this.TenDongDT.HeaderText = "Tên Dòng Điện Thoại";
+            this.TenDongDT.Name = "TenDongDT";
+            // 
+            // DonGia
+            // 
+            this.DonGia.DataPropertyName = "DonGia";
+            this.DonGia.HeaderText = "Đơn Giá";
+            this.DonGia.Name = "DonGia";
+            // 
+            // TonKho
+            // 
+            this.TonKho.DataPropertyName = "TonKho";
+            this.TonKho.HeaderText = "Tồn Kho";
+            this.TonKho.Name = "TonKho";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_xoadongdienthoai);
+            this.groupBox2.Location = new System.Drawing.Point(15, 425);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(447, 41);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ngừng bán dòng điện thoại";
+            // 
+            // btn_xoadongdienthoai
+            // 
+            this.btn_xoadongdienthoai.Location = new System.Drawing.Point(295, 12);
+            this.btn_xoadongdienthoai.Name = "btn_xoadongdienthoai";
+            this.btn_xoadongdienthoai.Size = new System.Drawing.Size(148, 23);
+            this.btn_xoadongdienthoai.TabIndex = 0;
+            this.btn_xoadongdienthoai.Text = "Xóa dòng điện thoại";
+            this.btn_xoadongdienthoai.UseVisualStyleBackColor = true;
+            this.btn_xoadongdienthoai.Click += new System.EventHandler(this.btn_xoadongdienthoai_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Số lượng điện thoại của hãng: ";
+            // 
+            // lb_soluongdt
+            // 
+            this.lb_soluongdt.AutoSize = true;
+            this.lb_soluongdt.Location = new System.Drawing.Point(161, 279);
+            this.lb_soluongdt.Name = "lb_soluongdt";
+            this.lb_soluongdt.Size = new System.Drawing.Size(16, 13);
+            this.lb_soluongdt.TabIndex = 6;
+            this.lb_soluongdt.Text = "...";
+            this.lb_soluongdt.Click += new System.EventHandler(this.lb_soluongdt_Click);
+            // 
+            // nud_tonkho
+            // 
+            this.nud_tonkho.Location = new System.Drawing.Point(73, 52);
+            this.nud_tonkho.Name = "nud_tonkho";
+            this.nud_tonkho.Size = new System.Drawing.Size(183, 20);
+            this.nud_tonkho.TabIndex = 6;
+            this.nud_tonkho.ValueChanged += new System.EventHandler(this.nud_tonkho_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 489);
+            this.Controls.Add(this.lb_soluongdt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGV_dt);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_chonhang);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV_dt)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_tonkho)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_chonhang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkb_cngia;
+        private System.Windows.Forms.Button btn_capnhat;
+        private System.Windows.Forms.DataGridView dataGV_dt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_gia;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_xoadongdienthoai;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenDongDT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DonGia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TonKho;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_soluongdt;
+        private System.Windows.Forms.NumericUpDown nud_tonkho;
+    }
+}
+
---
+++ QLDT_Winform_ADO/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace QLDT_Winform_ADO
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Server=.;Database=QUANLYDIENTHOAI_DTT;Integrated Security=true");
+        SqlDataAdapter adap;
+        DataTable dt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // hàm xuất thông tin cột được nhấn vào
+        private string[] currentRow(string cmd)
+        {
+            // hai biến cho đơn giá và tồn kho
+            string dg = dataGV_dt.CurrentRow.Cells["DonGia"].Value.ToString();
+            string tk = dataGV_dt.CurrentRow.Cells["TonKho"].Value.ToString();
+            // nếu cmd là get thì giá trị tham chiếu là mảng có giá trị (dg, tk)
+            if (cmd == "get")
+            {
+                return new string[] { dg, tk };
+            }
+            // ngược lại, nó sẽ chỉ là mảng rỗng
+            else
+            {
+                txt_gia.Text = dg;
+                nud_tonkho.Text = tk;
+                return new string[] {};
+            }       
+        }
+        // hàm điều khiển thông báo
+        private bool HandleAlerts(string type)
+        {
+            bool Check = false;
+            string alert = "";
+            // kiểm tra dạng thông báo
+            switch (type)
+            {
+                // nếu là update thì sẽ thay đổi thông báo dựa trên đơn giá
+                case "Update":
+                     alert = (Convert.ToInt64(txt_gia.Text) > Convert.ToInt64(currentRow("get")[0])) ?
+                        String.Format("Mặt hàng điện thoại {0} sẽ tăng giá?",
+                        dataGV_dt.CurrentRow.Cells["TenDongDT"].Value.ToString())
+                        :
+                        String.Format("Mặt hàng điện thoại {0} sẽ giảm giá?",
+                        dataGV_dt.CurrentRow.Cells["TenDongDT"].Value.ToString());
+
+                        if (txt_gia.Text == currentRow("get")[0])
+                            alert = String.Format("Mặt hàng điện thoại {0} vẫn sẽ giữ nguyên?",
+                            dataGV_dt.CurrentRow.Cells["TenDongDT"].Value.ToString());
+                    DialogResult dialogResult = MessageBox.Show(alert, "Thông báo", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        nud_tonkho.Focus();
+                        Check = true;
+                    }
+                    break;
+                // ngược lại, delete sẽ thay đổi thông báo phụ thuộc vào tồn kho
+                default:
+                    if (Convert.ToInt32(currentRow("get")[1]) > 0)
+                    {
+                        MessageBox.Show(String.Format(
+                       "Bạn không thể xóa vì mặt hàng điện thoại {0} vì vẫn còn tồn kho",
+                        dataGV_dt.CurrentRow.Cells["TenDongDT"].Value.ToString()));
+                    }
+                    else
+                    {
+                        alert = String.Format("Bạn có chắc chắn muốn xóa mặt hàng điện thoại {0}?",
+                        dataGV_dt.CurrentRow.Cells["TenDongDT"].Value.ToString());
+                        DialogResult dialogDelete = MessageBox.Show(alert, "Thông báo", MessageBoxButtons.YesNo);
+                        Check = (dialogDelete == DialogResult.Yes) ? true : false;
+                    }
+                    break;
+            }
+            return Check;
+        }
+        // hàm kích hoạt lệnh SQL (Update và Delete, vv...)
+        private void commandDataBTN(string type)
+        {
+            // Mã dòng ĐT 
+            string idData = dataGV_dt.CurrentRow.Cells["Ma"].Value.ToString();
+            // lệnh SQL và Thông báo
+            string query, alert = "";
+            query = (type == "Update") ?
+            String.Format("UPDATE DIENTHOAI SET DonGia={0}, " +
+                "TonKho={1} WHERE Ma='{2}'", txt_gia.Text, nud_tonkho.Text, idData) :
+            // Delete
+            String.Format("DELETE FROM DIENTHOAI WHERE Ma='{0}'", idData);
+            alert = (type == "Update") ? "Đã cập nhật thành công" : "Đã xóa thành công";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show(alert);
+                activeDataGridSQL();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Connection.Close();
+        }
+        // Hàm truyền dữ liệu SQL vào ComboBox
+        private void activeDataSQL()
+        {
+            string query = String.Format("SELECT * FROM HANGSX");
+            adap = new SqlDataAdapter(query, conn);
+            dt = new DataTable();
+            adap.Fill(dt);
+            // Lấy giá trị là mã hàng
+            cb_chonhang.DataSource = dt;
+            cb_chonhang.ValueMember = "MaHang";
+            cb_chonhang.DisplayMember = "MaHang";
+        }
+        // Hàm truyền dữ liệu SQL vào DataGridView
+        private void activeDataGridSQL()
+        {
+            string query = String.Format("SELECT Ma,TenDT,DonGia,TonKho FROM DIENTHOAI WHERE MaHang='{0}'", cb_chonhang.Text.Trim());
+            adap = new SqlDataAdapter(query, conn);
+            dt = new DataTable();
+            adap.Fill(dt);
+            dataGV_dt.DataSource = dt;
+            lb_soluongdt.Text = (dataGV_dt.RowCount - 1).ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            activeDataSQL();
+            cb_chonhang.SelectedIndex = -1;
+            btn_capnhat.Enabled = false;
+            btn_xoadongdienthoai.Enabled = false;
+            if (!checkb_cngia.Checked)
+            {
+                txt_gia.Enabled = false;
+            }
+        }
+        // Kết nối sự kiện với activeDataGridSQL để in ra bảng dữ liệu chính xác của từng loại mã 
+        private void cb_chonhang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            activeDataGridSQL();
+        }
+
+        private void btn_capnhat_Click(object sender, EventArgs e)
+        {
+            // Nếu HandleAlerts trả về true thì hàm kích hoạt lệnh SQL sẽ được triển khai
+            if (HandleAlerts("Update"))
+            {
+                commandDataBTN("Update");  
+            } 
+        }
+
+        private void dataGV_dt_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dataGV_dt_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Sửa lỗi ở phần cập nhật và xóa
+            currentRow("show");
+            btn_xoadongdienthoai.Enabled = true;
+            btn_capnhat.Enabled = false;
+        }
+
+        private void checkb_cngia_CheckedChanged(object sender, EventArgs e)
+        {
+            txt_gia.Enabled = (checkb_cngia.Checked) ? true : false;
+        }
+
+        private void lb_soluongdt_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_xoadongdienthoai_Click(object sender, EventArgs e)
+        {
+            if (HandleAlerts("Delete")) commandDataBTN("Delete");  
+        }
+
+        private void txt_gia_TextChanged(object sender, EventArgs e)
+        {
+            if (txt_gia.Text != currentRow("get")[0])
+            {
+                btn_capnhat.Enabled = true;
+                checkb_cngia.Checked = true;
+            }
+        }
+        private void nud_tonkho_ValueChanged(object sender, EventArgs e)
+        {
+            if (nud_tonkho.Text != currentRow("get")[1])
+            {
+                btn_capnhat.Enabled = true;
+            }
+        }
+
+    }
+}
